3b59d835385ede3524626a74c046318d12b1ca1e
Yura <yura51900@gmail.com>
2018-09-08 18:08:34 +0300

    updated version of constraint layout library and some refactoring

--- app/src/main/java/com/example/searchmeal/MainActivity.java
+++ app/src/main/java/com/example/searchmeal/MainActivity.java
@@ -10,7 +10,6 @@ import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
-import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
